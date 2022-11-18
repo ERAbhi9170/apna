b4341e60929298a5670a142e35a77d0674b77226
Abhishek <er.abhi9170@gmail.com>
2022-11-18 23:28:22 +0530

    add multipal card

--- components/DreamJob/index.js
+++ components/DreamJob/index.js
@@ -1,8 +1,51 @@
+import Image from 'next/image';
 import DreamJobCard from "../DreamJobCard";
+import one from '../../public/10.svg';
+import two from '../../public/2.svg';
+import three from '../../public/3.svg'
+import shutterstock1 from '../../public/shutterstock1.png';
+import shutterstock2 from '../../public/shutterstock2.png';
+import shutterstock3 from '../../public/shutterstock3.png';
+import Vector144 from '../../public/Vector144.png';
+import Vector145 from '../../public/Vector145.png';
+
 const DreamJob =()=>{
+    
+   const STEP =[
+      {
+         number:one,
+         title:'Sign-up for the course',
+         awatar:shutterstock1,
+         background:'bg-yellow-500'
+      },
+      {
+         number:two,
+         title:'Get trained by industry experts',
+         awatar:shutterstock2,
+         background:'bg-amber-500'
+      },
+      {
+         number:three,
+         title:'Give interviews & get dream job',
+         awatar:shutterstock3,
+         background:'bg-teal-500'
+      }
+   ]
+
    return(
-            <div className=" bg-primary min-h-screen text-center">
-                        <DreamJobCard/>
+            <div className="flex flex-col items-center bg-primary h-screen lg:flex-row justify-center">
+               {
+                  STEP.map((item,index)=>{
+                   return(
+                     <div key={item.title}>
+                     {/* {index===1 && <Image src={Vector144} alt='vector144' className='absolute left-10' />} */}
+                     <DreamJobCard  data={item} i={index} />
+                     {/* {index===1 && <Image src={Vector145} alt='vector145'className='absolute right-11' />} */}
+                     </div>
+                   )
+                  } )
+               }
+                       
             </div>
    )         
 }
--- components/DreamJobCard/index.js
+++ components/DreamJobCard/index.js
@@ -1,19 +1,23 @@
 
 import Image from 'next/image';
-import one from '../../public/10.svg'
-import shutterstock1 from '../../public/shutterstock1.jpg'
+import Vector144 from '../../public/Vector144.png';
+import Vector145 from '../../public/Vector145.png';
 
-const DreamJobCard = () => {
-            return (
-                        <div className=' flex align items-center justify-center pt-12 '>
-                                    <div className='bg-secondary w-48 h-20 border-dashed border-2 border-golden rounded-2xl relative'>
-                                                <Image src={one} alt='1' width={32} height={64} className='absolute -top-9 left-0' />
-                                                <p className='pl-12 text-start pt-3 text-white'>Sign-up for the course</p>
-                                    </div>
-                                    <div className='w-16 h-16'>
-                                                <Image className='rounded-full ml-3' src={shutterstock1} alt='shutterstock1' />
-                                    </div>
-                        </div>
-            )
+
+const DreamJobCard = ({ data ,i }) => {
+     const { number, title, awatar,background } = data;
+     return (
+          <div className={`flex  align items-center justify-center pt-12 ${i==1?'flex-row-reverse ml-9 sm:ml-20 lg:flex-row mr-9':''}`}>
+               <div className='bg-secondary w-48 h-20 border-dashed border-2 border-golden rounded-2xl relative'>
+               {i===1 && <Image src={Vector144} alt='vector144' className='absolute -left-20 -top-12 lg:top-16 lg:-rotate-90' />}
+               {i===1 && <Image src={Vector145} alt='vector145'className='absolute top-20 right-2 lg:-top-16 lg:-right-16 lg:-rotate-90' />}
+                    <Image src={number} alt='1' width={32} height={64} className='absolute -top-7 left-1' />
+                    <p className='pl-12 text-start pt-3 text-white'>{title}</p>
+               </div>
+               <div className='w-16 h-16 lg:-translate-x-5'>
+                    <Image className ={`rounded-full m-3 ${background} ${i==1?'p-1':''}`}  src={awatar} alt='shutterstock1' height={46}/>
+               </div>
+          </div>
+     )
 }
 export default DreamJobCard;
